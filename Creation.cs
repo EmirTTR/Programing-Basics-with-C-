7f5bc7a2c305b3a2c6df9dd8d571ab2ab810b4da
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-04-06 14:26:15 +0300

    ProjectCreation

---
+++ Creation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Projects_Creation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string architectName = Console.ReadLine();
+            int numProject = int.Parse( Console.ReadLine());
+            int numHours = numProject * 3;
+
+            Console.WriteLine($"The architect {architectName} will need {numHours} hours to complete {numProject} project/s.");
+        }
+    }
+}
