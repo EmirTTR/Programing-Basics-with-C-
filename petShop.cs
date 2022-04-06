2707b24b16ac50d3896b3ec8cd53ec6585735c80
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-04-06 14:24:49 +0300

    PetShop BasicCode

---
+++ petShop.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PetShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Kuche =int.Parse(Console.ReadLine());
+            int Kotka = int.Parse(Console.ReadLine());
+            double result = Kuche * 2.50 + Kotka * 4;
+            Console.WriteLine($"{result} lv.");
+
+
+
+
+
+
+        }
+    }
+}
