030e8eb1de3dffca0e28ed9e4d3d791dd6ef5540
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-04-07 19:11:55 +0300

    Yard Greening

---
+++ Yard Greening.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Yard_Greening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double cenaNaKvMetar = 7.61;
+            double discount = 0.18;
+            double broi = double.Parse(Console.ReadLine());
+            double kraenRezultat = broi * cenaNaKvMetar;
+            double finaldiscount =(kraenRezultat * discount);
+            Console.WriteLine("The final price is:"+ (kraenRezultat - finaldiscount) +" lv." );
+            Console.WriteLine($"The discount is: {finaldiscount} lv.");
+        }
+    }
+}
