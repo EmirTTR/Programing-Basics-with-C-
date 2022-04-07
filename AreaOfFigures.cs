2edc8f56e0a39ffbfbd75cd0a9ecbd02310578bd
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-04-07 19:12:45 +0300

    Area of Figures

---
+++ AreaOfFigures.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Area_of_Figures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string figure = Console.ReadLine();
+            double result = 0.0;
+           
+            if (figure=="square")
+            {
+                double side=double.Parse(Console.ReadLine());
+                
+                result = side * side;
+                Console.WriteLine($"{result:F3}");
+                
+
+            }
+            else if (figure=="rectangle")
+            {
+                double sideA = double.Parse(Console.ReadLine());
+                double sideB = double.Parse(Console.ReadLine());
+
+                result = sideA * sideB;
+                Console.WriteLine($"{result:F3}");
+                
+            }
+            else if (figure == "circle")
+            {
+                double radius = double.Parse(Console.ReadLine());
+                
+                result = Math.PI*radius*radius;
+                Console.WriteLine($"{result:F3}");
+                
+            }
+            else if (figure == "triangle")
+            {
+                double side = double.Parse(Console.ReadLine());
+                double area = double.Parse(Console.ReadLine());
+               
+                result = 0.5 * area * side;
+                Console.WriteLine($"{result:F3}");
+
+
+
+
+            }
+            
+
+        }
+    }
+}
