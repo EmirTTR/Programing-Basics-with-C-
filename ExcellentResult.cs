06ca51a79a907d7dd1fcd961584ea9f67ec5ae0d
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-04-08 13:33:47 +0300

    Excellent Result

---
+++ ExcellentResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Excellent_Result
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 = double.Parse(Console.ReadLine());
+            if(num1>=5.50)
+            {
+                Console.WriteLine("Excellent!");
+            }  
+            
+                            
+        }
+    }
+}
