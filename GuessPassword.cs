20d6c6a1434b33e176534c4bbf2f3746bd14c84e
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-04-08 13:34:48 +0300

    Guess Password

---
+++ GuessPassword.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GuesPassword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "s3cr3t!P@ssw0rd";
+            string guess = Console.ReadLine();
+            if(guess== password)
+
+            {
+                Console.WriteLine("Welcome!");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
