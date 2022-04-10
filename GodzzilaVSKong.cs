51a45a141244167356d97c18aa84a0eb68a57f57
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-04-10 19:12:07 +0300

    Godzilla vs Kong

---
+++ GodzzilaVSKong.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace GodzzilavsKong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double movieBudget = double.Parse(Console.ReadLine());
+            int numStatists = int.Parse(Console.ReadLine());
+            double clothesPrice = double.Parse(Console.ReadLine());
+            double decore = movieBudget * 0.1;
+            double totalpriceforClothes = numStatists * clothesPrice;
+
+            if (numStatists > 150)
+            {
+                totalpriceforClothes = totalpriceforClothes * 0.90;
+            }
+            double totalMoney = decore + totalpriceforClothes;
+
+            if (totalMoney>movieBudget)
+            {
+                double nedostig = totalMoney - movieBudget;
+                Console.WriteLine($"Not enough money!");
+                Console.WriteLine($"Wingard needs {nedostig:f2} leva more."); 
+            }
+            else 
+            {
+                double poveche = movieBudget - totalMoney;
+                Console.WriteLine($"Action!");
+                Console.WriteLine($"Wingard starts filming with {poveche:f2} leva left.");
+
+            }
+        
+        }
+    }
+}
