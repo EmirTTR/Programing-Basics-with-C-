1c89f04c7ea974434dfe8b2f70b1d3d83cc14925
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-04-11 11:38:37 +0300

    Sum Secconds

---
+++ SumSecconds.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SumSecconds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstPerson = int.Parse(Console.ReadLine());
+            int secondPerson = int.Parse(Console.ReadLine());
+            int therdPerson = int.Parse(Console.ReadLine());
+            int allTime = firstPerson + secondPerson + therdPerson;
+
+            int minutes = 0;
+            int seconds = 0;
+
+            if (allTime>=120)
+            {
+                minutes = 2;
+                seconds = allTime - 120;
+            }
+            else if (allTime>=60)
+            {
+                minutes = 1;
+                seconds = allTime - 60;
+            }
+            else
+            {
+                seconds = allTime;
+            }
+            Console.WriteLine($"{minutes}:{seconds:d2}");
+            
+          
+
+        }
+    }
+}
