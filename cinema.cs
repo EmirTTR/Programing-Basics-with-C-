0a18cf137a8cb603380d053c94ee664d4c64e8cd
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-04-12 13:38:44 +0300

    Cinema

---
+++ cinema.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            int rows = int.Parse(Console.ReadLine());
+            int colums = int.Parse(Console.ReadLine());
+            double income = 0.00;
+            double price = 0.00;
+            switch (type)
+            {
+                case "Premiere":
+                    price = 12.00;
+                    break;
+                default:
+                    break;
+                case "Normal":
+                    price = 7.50;
+                    break;
+                case "Discount":
+                    price = 5.00;
+                    break; 
+            }
+            income = price * rows * colums;
+            Console.WriteLine($"{income:F2}");
+
+        }
+    }
+}
