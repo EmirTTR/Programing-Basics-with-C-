a546693a06f1efb73e2f6741551f256b4e1d0c2f
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-04-13 15:03:38 +0300

    Cinema Ticket

---
+++ kinoTicket.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Working_Hours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string day = Console.ReadLine();
+
+                {
+                    if (day == "Monday")
+                    {
+                        Console.WriteLine("12");
+                    }
+                    if (day == "Tuesday")
+                    {
+                        Console.WriteLine("12");
+                    }
+                    if (day == "Wednesday")
+                    {
+                        Console.WriteLine("14");
+                    }
+                    if (day == "Thursday")
+                    {
+                        Console.WriteLine("14");
+                    }
+                    if (day == "Friday")
+                    {
+                        Console.WriteLine("12");
+                    }
+                    if (day == "Saturday")
+                    {
+                        Console.WriteLine("16");
+                    }
+                     if (day == "Sunday")
+                    {
+                        Console.WriteLine("16");
+                    }
+
+                }
+             
+        }
+    }
+}
