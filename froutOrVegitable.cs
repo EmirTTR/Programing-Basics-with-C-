985a9daa0468d1d63611785c8cac6af3afee0f5f
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-04-14 12:30:30 +0300

    Fruit or vegetable

---
+++ froutOrVegitable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FruitorVegitable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            string v = word;
+
+            switch (word)
+            {
+                case "banana":
+                case "apple":
+                case "kiwi":
+                case "cherry":
+                case "lemon":
+                case "grapes":
+
+                    Console.WriteLine("fruit");
+                        break;
+                case "tomato":
+                case "cucumber":
+                case "pepper":
+                case "carrot":
+                    Console.WriteLine("vegetable");
+                    break;
+                default:
+                    Console.WriteLine("unknown");
+                    
+                    break;
+            }
+
+        }
+    }
+
+}
+    
+
