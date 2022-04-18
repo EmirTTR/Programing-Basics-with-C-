2216640f65186e4aafeae5bf759dd99f94949e02
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-04-18 18:55:09 +0300

    Clever Lily

---
+++ cleverLily.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace CleverLily
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lilyAge = int.Parse(Console.ReadLine());
+            double laundryPrice = double.Parse(Console.ReadLine());
+            int toyPrice = int.Parse(Console.ReadLine());
+           
+            int money = 0;
+            int neededMoney = 0;
+            int toys = 0;
+            
+            for (int i = 1; i <= lilyAge; i++)
+            {
+                money += 5;
+
+                if (i%2 == 0)
+                {
+                    neededMoney += money-1;
+                }
+                else
+                {  
+                    toys++;
+                }
+
+            }
+                   neededMoney+= toys * toyPrice;
+
+            if (neededMoney >= laundryPrice)
+            {
+
+                Console.WriteLine($"Yes! {neededMoney - laundryPrice:F2}");
+            }
+            else
+            {
+
+                Console.WriteLine($"No! {laundryPrice - neededMoney:F2}");
+            }
+            Console.WriteLine(Math.Floor(laundryPrice));
+        }
+    }
+}
