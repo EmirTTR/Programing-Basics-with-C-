090af5fa3721a67d4bc2ba95d7636e7b1ab2adfc
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-04-21 14:58:33 +0300

    Left right sum

---
+++ leftRightSum.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace LeftRightSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int leftSum = 0;
+            int rightdSum = 0;
+
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                leftSum += num;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                int num2 = int.Parse(Console.ReadLine());
+                rightdSum += num2;
+
+            }
+            if (leftSum==rightdSum)
+            {
+                Console.WriteLine("Yes, sum = " + leftSum);
+            }
+            else
+            {
+                int difference = Math.Abs(leftSum - rightdSum);
+                Console.WriteLine("No, diff = " + difference);
+            }
+        }
+    }
+}
+    
+
