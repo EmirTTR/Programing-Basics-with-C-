78862c480b82529ef039cc51a4b63efe5fb9f3c1
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-04-22 12:24:46 +0300

    Number N to 1

---
+++ numberNTo1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Number_n_to_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = n; i >=1; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
