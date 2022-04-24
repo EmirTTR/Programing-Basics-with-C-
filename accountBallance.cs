15c2bcd00ae5ff8776d428cb84bbed9cd9804de9
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-04-24 18:34:21 +0300

    Account Ballance

---
+++ accountBallance.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Account_Balance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ballance = Console.ReadLine();
+            double finalBallance = double.Parse(ballance);
+
+            double sum = 0;
+
+
+            while (ballance != "NoMoreMoney")
+            {
+                double money = double.Parse(ballance);
+                
+
+                if (money<0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+                
+                Console.WriteLine($"Increase: {money:f2}");
+                sum+=money;
+                
+                ballance = Console.ReadLine();
+            }
+            Console.WriteLine($"Total: {sum:f2} ");
+        }
+    }
+}
