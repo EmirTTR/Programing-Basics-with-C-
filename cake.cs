a15c6becd29d989b90a09df2dc4be18b48f39450
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-04-26 12:46:50 +0300

    Cake

---
+++ cake.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Cake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cakeLenght = int.Parse(Console.ReadLine());
+            int cakeWidth = int.Parse(Console.ReadLine());
+            int cakePeace = cakeWidth * cakeLenght;
+            while (cakePeace>0)
+            {
+                string input = Console.ReadLine();
+                if (input=="STOP")
+                {
+                    break;
+                }
+                int guestPeaces = int.Parse(input);
+                cakePeace -= guestPeaces;
+
+            }
+                if (cakePeace>=0)
+                {
+                   
+                    Console.WriteLine($"{cakePeace} pieces are left.");
+                }
+                else 
+                {
+                    Console.WriteLine($"No more cake left! You need {Math.Abs(cakePeace)} pieces more.");
+                }
+         
+       
+        }
+
+	
+    }
+}
