d46183fb52861fa3fb6f4397e3d752ca9113a152
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-04-26 12:46:25 +0300

    Any's Book

---
+++ any'sBook.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Any_sBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string aBook = Console.ReadLine();
+            int tryCounter = 0;
+            string books = Console.ReadLine();
+    
+            while (books != "No More Books")
+            {
+                
+                
+                if (aBook == books)
+                {
+                    Console.WriteLine($"You checked {tryCounter} books and found it.");
+                    break;
+                }
+                
+                tryCounter +=1;
+                books = Console.ReadLine();
+
+            }
+                if (books=="No More Books")
+                {
+                    Console.WriteLine($"The book you search is not here!");
+                    Console.WriteLine($"You checked {tryCounter} books.");
+                    
+                }
+            
+
+        }
+    }
+}
