b89c293512711739ce1f2f3758302e25c7d55ff9
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-04-28 13:16:40 +0300

    Other

---
+++ other.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace pesho
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string movieName = Console.ReadLine();
+            double movieTime = Double.Parse(Console.ReadLine());
+            double breakTime = Double.Parse(Console.ReadLine());
+
+            double lunchTime = breakTime * 1.0 / 8;
+            double relaxTime = breakTime * 1.0 / 4;
+
+            double freeTime = breakTime - lunchTime - relaxTime;
+            double needTime = movieTime - freeTime;
+
+            if (freeTime >= movieTime)
+            {
+                freeTime = movieTime;
+                Console.WriteLine($"You have enough time to watch {movieName} and left with {movieTime} minutes free time.");
+            }
+            if (freeTime < movieTime)
+            {
+                Console.WriteLine($"You don't have enough time to watch {movieName}, you need {freeTime} more minutes.");
+            }
+        }
+    }
+}
