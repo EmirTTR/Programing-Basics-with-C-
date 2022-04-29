96ad9d0a0a2bad7b9e66892530f316d0113ff1b4
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-04-29 12:57:16 +0300

    Building

---
+++ building.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace building
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nums = int.Parse(Console.ReadLine());
+            int roomsByFloor = int.Parse(Console.ReadLine());
+                        
+            for (int j = nums; j >=1; j--)
+            {
+                for (int i=0;i<roomsByFloor;i++)
+                {
+                    if (j==nums)
+                    {
+                        Console.Write($"L{j}{i} ");
+                        continue;
+                    }
+
+
+                    if (j%2==0)
+                    {
+                        Console.Write($"O{j}{i} ");
+                    }
+
+
+                    else
+                    {
+                       Console.Write($"A{j}{i} ");
+                    }
+                    
+                }
+                        
+                Console.WriteLine();
+            }
+        }
+    }
+}
