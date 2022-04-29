9e3c83e23ce54e519225aa803966f281213f91a4
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-04-29 12:58:13 +0300

    Combinations

---
+++ Combinations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Combinations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input =int.Parse( Console.ReadLine());
+            int combinations = 0;
+            for (int x1 = 0; x1 <=input; x1++)
+            {
+                for (int x2 = 0; x2 <=input; x2++)
+                {
+                    
+                    
+                    for (int x3 = 0; x3 <= input; x3++)
+                    {
+                        if (x1+x2+x3==input)
+                        {
+                           
+                                input = x1 + x2 + x3;
+                                
+                                combinations++;
+                            
+                        }
+                    }
+                }
+            }
+                                Console.WriteLine(combinations);
+        }
+    }
+}
