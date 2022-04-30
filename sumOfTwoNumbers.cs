e918bcab0e4b7ad1c8cd72b280da1b4d04042ef4
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-04-30 12:59:35 +0300

    Sum of two numbers

---
+++ sumOfTwoNumbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SumofTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+            int magic = int.Parse(Console.ReadLine());
+            int combinations = 0;
+
+            for (int i = start; i <= end; i++)
+            {
+                for (int j = start; j <= end; j++)
+                {
+                    int sum = i + j;
+                    combinations++;
+                    if (sum == magic)
+                    {
+                        Console.WriteLine($"Combination N:{combinations} ({i} + {j} = {magic})");
+                        return;
+                    }
+
+
+                }
+            }
+           
+                Console.WriteLine($"{combinations} combinations - neither equals {magic}");
+            
+        }
+    }
+}
