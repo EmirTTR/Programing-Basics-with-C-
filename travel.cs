cf3adaaaf981a0eb857ba4fa9e8141d48bb9325c
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-04-30 13:00:05 +0300

    Travel

---
+++ travel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Travel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string go = Console.ReadLine();
+            while (go!="End")
+            {
+                double budget = double.Parse(Console.ReadLine());
+                double saved = 0;
+                while (saved<budget)
+                {
+
+                    saved += double.Parse(Console.ReadLine());
+                }
+                    Console.WriteLine($"Going to {go}!");
+                go = Console.ReadLine();
+            }
+        }
+    }
+}
