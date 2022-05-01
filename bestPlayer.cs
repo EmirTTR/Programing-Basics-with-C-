da103361eabbcf46b4dbf8ffe160ebebc5ff8fc2
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-05-01 11:04:30 +0300

    Best Player

---
+++ bestPlayer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Temp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string bestPlayer =Console.ReadLine();
+            int score = int.Parse(Console.ReadLine());
+            int bestScore = int.MinValue;
+            
+            while (bestPlayer != "END")
+            {
+                bestPlayer = Console.ReadLine();
+                score = int.Parse(Console.ReadLine());
+                if (score > bestScore)
+                {
+                    bestScore = score;
+                    
+                }
+
+                if (bestScore >= 10)
+                {
+                    continue;
+                }
+            }
+
+            Console.WriteLine($"{bestPlayer} is the best player!");
+            if (bestScore >= 3)
+            {
+                Console.WriteLine($"He has scored {bestScore} goals and made a hat-trick !!!");
+            }
+            else
+            {
+                Console.WriteLine($"He has scored {bestScore} goals.");
+            }
+        }
+    }
+}
