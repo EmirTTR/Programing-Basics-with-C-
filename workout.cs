a45b33eff7e783c3d4594a7b4e70c1574d067fe8
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-05-02 10:17:12 +0300

    Workout

---
+++ workout.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _04.Workout
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int needed = 1000;
+            int numDays = int.Parse(Console.ReadLine());
+            double numKilometers = double.Parse(Console.ReadLine());
+            double all = numKilometers;
+            
+            for (int i = 0; i < numDays; i++)
+            {
+                double kilometers = double.Parse(Console.ReadLine());
+                numKilometers = numKilometers + numKilometers * (kilometers / 100);
+                all += numKilometers;
+            }
+
+            if (all >= needed)
+            {
+                Console.WriteLine($"You've done a great job running {Math.Ceiling(all - needed)} more kilometers!");
+            }
+            else
+            {
+                Console.WriteLine($"Sorry Mrs. Ivanova, you need to run {Math.Ceiling(needed - all)} more kilometers");
+            }
+        }
+    }
+}
