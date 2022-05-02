a17087c18d66ad6bb1579a535300137b0358e0a7
Emir Edjevit Tair <100964010+EmirTTR@users.noreply.github.com>
2022-05-02 10:16:50 +0300

    Unique pins

---
+++ uniquePins.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace uniquePins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+
+            for (int one = 2; one <= first; one += 2)
+            {
+                for (int two = 2; two <= second; two++)
+                {
+                    for (int three = 2; three <= third; three += 2)
+                    {
+                        if (two == 2 || two == 3 || two == 5 || two == 7)
+                        {
+                            Console.WriteLine($"{one} {two} {three}");
+
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
